2be29ea41732a746802b1ab32923c145ebf31ade
Greiza García <159574736+GreizaG@users.noreply.github.com>
2024-06-17 12:53:42 +0000

    Mis cursos y profesores

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,15 +3,32 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+		<nav className="navbar navbar-expand-lg bg-white">
+			<div className="container-fluid d-flex justify-content-between">
+				<div>
+					<span>Acamedia ACUA</span>
+					<span className="ms-2">Greiza García</span>
+				</div>
+				<div>
+					<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+						<i className="fa-solid fa-bars"></i>
+					</button>
+					<div className="collapse navbar-collapse" id="navbarScroll">
+						<ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
+							<li className="nav-item">
+								<span className="me-4">Inicio</span>
+							</li>
+							<li className="nav-item">
+								<span className="me-4">Información personal</span>
+							</li>
+							<li className="nav-item">
+								<span className="me-4">Facturación</span>
+							</li>
+						</ul>
+						<button className="btn btn-warning">Salir</button>
+					</div>
+				</div>
 			</div>
-		</nav>
+		</nav >
 	);
 };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,13 +3,60 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+	<div className="container-fluid">
+		<div className="mb-3 mt-3 bg-light rounded-3">
+			<div className="container-fluid">
+				<h4 className="py-2 fw-bold">Mis Cursos</h4>
+			</div>
+		</div>
+		<div className="continer-fluid d-flex">
+			<div>
+				<div className="card" style={{ width: '16rem' }}>
+					<div className="card-header fw-bold">
+						Guitarra Eléctrica
+					</div>
+					<img src="https://th.bing.com/th/id/OIP.nyAhZ9G7WJaZiFMpeW5GMQHaHV?rs=1&pid=ImgDetMain" className="card-img-top" />
+					<div className="card-body">
+						<table className="table table-borderless">
+							<tr>
+								<td className="text-secondary text-5 fw-semibold">Profesor</td>
+								<td className="fw-lighter">Roberto Antillón</td>
+							</tr>
+						</table>
+					</div>
+				</div>
+			</div>
+			<div className="mt-auto ms-3">
+				<button type="button" className="btn btn-warning badge text-wrap fw-lighter text-dark text-start" style={{ width: '8rem' }}>Solicitar inscribir nuevo curso<i className="ms-2 fa-solid fa-angles-right"></i></button>
+			</div>
+		</div>
+		<div className="mb-3 mt-3 bg-light rounded-3">
+			<div className="container-fluid">
+				<h4 className="py-2 fw-bold">Mis Profesores</h4>
+			</div>
+		</div>
+		<div>
+			<div className="card" style={{ width: '16rem' }}>
+				<div className="container card-header fw-bold">
+					<div className="row">
+						<div className="col">
+							<span>Roberto Antillón</span>
+						</div>
+						<div className="col">
+							<button type="button" className="btn btn-small btn-warning badge fw-lighter text-dark text-start">Ver más<i className="ms-2 fa-solid fa-angles-right"></i></button>
+						</div>
+					</div>
+				</div>
+				<img src="https://i.pinimg.com/236x/31/9b/08/319b08d0fe5624ca5078ac762fb78634.jpg?nii=t" className="card-img-top" />
+				<div className="card-body">
+					<table className="table table-borderless">
+						<tr>
+							<td className="text-secondary text-5 fw-semibold">Curso</td>
+							<td className="fw-lighter">Guitarra Eléctrica</td>
+						</tr>
+					</table>
+				</div>
+			</div>
+		</div>
 	</div>
 );
