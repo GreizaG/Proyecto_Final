2aa1a0bed7a0f25cc7ab2508545064c5532933cb
Greiza García <159574736+GreizaG@users.noreply.github.com>
2024-06-18 23:05:09 +0000

    Home estudiantes con fotos

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../../img/logo.png";
 
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-expand-lg bg-white">
-			<div className="container-fluid d-flex justify-content-between">
+			<div className="container-fluid d-flex justify-content-between align-content-center">
 				<div>
-					<span>Acamedia ACUA</span>
-					<span className="ms-2">Greiza García</span>
+					<img className="logo" src={logo}/>
+					<span className="ms-2 fs-5">Greiza García</span>
 				</div>
 				<div>
 					<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
@@ -16,16 +17,16 @@ export const Navbar = () => {
 					<div className="collapse navbar-collapse" id="navbarScroll">
 						<ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
 							<li className="nav-item">
-								<span className="me-4">Inicio</span>
+								<span className="me-4 fs-5">Inicio</span>
 							</li>
 							<li className="nav-item">
-								<span className="me-4">Información personal</span>
+								<span className="me-4 fs-5">Información personal</span>
 							</li>
 							<li className="nav-item">
-								<span className="me-4">Facturación</span>
+								<span className="me-4 fs-5">Facturación</span>
 							</li>
 						</ul>
-						<button className="btn btn-warning">Salir</button>
+						<button className="btn btn-warning rounded-pill">Salir</button>
 					</div>
 				</div>
 			</div>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,17 +1,18 @@
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import professor1 from "../../img/professor1.jpeg";
 
 export const Home = () => (
 	<div className="container-fluid">
-		<div className="mb-3 mt-3 bg-light rounded-3">
-			<div className="container-fluid">
-				<h4 className="py-2 fw-bold">Mis Cursos</h4>
+		<div className="d-flex align-content-center justify-content-center mb-3 mt-3 py-4 jumbo rounded-3">
+			<div className="container-fluid align-content-center ms-4">
+				<h4 className="py-2 ps-5 fw-bold">Mis Cursos</h4>
 			</div>
 		</div>
-		<div className="continer-fluid d-flex">
+		<div className="continer-fluid d-flex ps-5 ms-4">
 			<div>
-				<div className="card" style={{ width: '16rem' }}>
+				<div className="card" style={{ width: '20rem' }}>
 					<div className="card-header fw-bold">
 						Guitarra Eléctrica
 					</div>
@@ -27,32 +28,57 @@ export const Home = () => (
 				</div>
 			</div>
 			<div className="mt-auto ms-3">
-				<button type="button" className="btn btn-warning badge text-wrap fw-lighter text-dark text-start" style={{ width: '8rem' }}>Solicitar inscribir nuevo curso<i className="ms-2 fa-solid fa-angles-right"></i></button>
+				<button type="button" className="btn btn-warning badge text-wrap fw-lighter text-dark text-start rounded-pill" style={{ width: '8rem' }}>Solicitar inscribir nuevo curso<i className="ms-2 fa-solid fa-angles-right"></i></button>
 			</div>
 		</div>
-		<div className="mb-3 mt-3 bg-light rounded-3">
-			<div className="container-fluid">
-				<h4 className="py-2 fw-bold">Mis Profesores</h4>
+		<div className="d-flex align-content-center justify-content-center mb-3 mt-3 py-4 jumbo rounded-3">
+			<div className="container-fluid align-content-center ms-4">
+				<h4 className="py-2 ps-5 fw-bold">Mis Profesores</h4>
 			</div>
 		</div>
-		<div>
-			<div className="card" style={{ width: '16rem' }}>
+		<div className="continer-fluid d-flex ps-5 ms-4">
+			<div className="card" style={{ width: '20rem' }}>
 				<div className="container card-header fw-bold">
 					<div className="row">
 						<div className="col">
 							<span>Roberto Antillón</span>
 						</div>
 						<div className="col">
-							<button type="button" className="btn btn-small btn-warning badge fw-lighter text-dark text-start">Ver más<i className="ms-2 fa-solid fa-angles-right"></i></button>
+							<button type="button" className="btn btn-small btn-warning badge fw-lighter text-dark text-start rounded-pill">Ver más<i className="ms-2 fa-solid fa-angles-right"></i></button>
 						</div>
 					</div>
 				</div>
-				<img src="https://i.pinimg.com/236x/31/9b/08/319b08d0fe5624ca5078ac762fb78634.jpg?nii=t" className="card-img-top" />
+				<img src={professor1} className="card-img-top" />
 				<div className="card-body">
 					<table className="table table-borderless">
 						<tr>
 							<td className="text-secondary text-5 fw-semibold">Curso</td>
 							<td className="fw-lighter">Guitarra Eléctrica</td>
+						</tr>
+					</table>
+				</div>
+			</div>
+		</div>
+		<div className="d-flex align-content-center justify-content-center mb-3 mt-3 py-4 jumbo rounded-3">
+			<div className="container-fluid align-content-center ms-4">
+				<h4 className="py-2 ps-5 fw-bold">Mi próximo pago</h4>
+			</div>
+		</div>
+		<div className="continer-fluid d-flex ps-5 ms-4">
+			<div className="card" style={{ width: '20rem' }}>
+				<div className="container d-flex card-body">
+					<table className="table table-borderless">
+						<tr className="mb-2">
+							<td className="text-secondary text-5 fw-semibold">Fecha pago</td>
+							<td className="fw-lighter">30 Junio 2024</td>
+						</tr>
+						<tr className="mb-2">
+							<td className="text-secondary text-5 fw-semibold">Monto a pagar</td>
+							<td className="fw-lighter">₡ 75.000</td>
+						</tr>
+						<tr>
+							<td className="text-secondary text-5 fw-semibold">Fecha vencimiento</td>
+							<td className="fw-lighter">05 Julio 2024</td>
 						</tr>
 					</table>
 				</div>
